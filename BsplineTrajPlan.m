ad9777b22697a8332b1b62c65737441b35c6ccd3
Ma Xihan <31639301+MXHsj@users.noreply.github.com>
2021-03-29 23:11:45 -0400

    tunning trajectory plan params

--- BsplineTrajPlan.m
+++ BsplineTrajPlan.m
@@ -21,8 +21,8 @@ z = reshape(wp_raw(3,4,:),size(wp_raw,3),1)*1000;
 coplane_x = mean(x);
 coplane_y = y;
 coplane_z = z;
-samples = 30;
-path = polyfit(coplane_y, coplane_z, 4);
+samples = 10;
+path = polyfit(coplane_y, coplane_z, 3);
 cont_x = coplane_x*ones(samples,1);
 cont_y = linspace(y(1),y(end),samples)';
 cont_z = polyval(path,cont_y);
@@ -31,26 +31,24 @@ dpath = polyder(path);
 calc_len = @(x) sqrt(1 + polyval(dpath,x).^2);
 path_len = integral(calc_len,min(cont_y),max(cont_y));
 
-% velocity boundary conditions
-lin_vel = 3.4;
-x_vel_bc = zeros(samples,1);
-y_vel_bc = -lin_vel*cos(atan(polyval(dpath,cont_y)));
-y_vel_bc(1) = 0; y_vel_bc(end) = 0;
-z_vel_bc = -lin_vel*sin(atan(polyval(dpath,cont_y)));
-z_vel_bc(1) = 0; z_vel_bc(end) = 0;
-vel_bc = [x_vel_bc, y_vel_bc, z_vel_bc]';
-
 %% calculate trajectory
 freq = 10;
-end_time = 0.3*path_len;
+end_time = 0.25*path_len;
 timePoints = linspace(0,end_time,samples);
+tInterval = [0 end_time];
 tvec = 0:1/freq:end_time;
 
-[s,sd,sdd] = quinticpolytraj([cont_x, cont_y, cont_z]', ...
-                             timePoints,tvec, ...
-                             'VelocityBoundaryCondition',vel_bc);
+[s,sd,sdd] = bsplinepolytraj([cont_x, cont_y, cont_z]',tInterval,tvec);
 
 %% visualization
+% rotations = tform2quat(T);
+% translations = tform2trvec(T);
+% figure
+% plotTransforms(translations,rotations,'FrameSize',0.008)
+% grid on
+% xlabel('X [mm]')
+% ylabel('Y [mm]')
+
 figure
 % plot3(s(1,:),s(2,:),s(3,:),'.b')
 plot(s(2,:),s(3,:),'.b')
@@ -60,10 +58,10 @@ plot(cont_y,cont_z,'*g')
 grid on
 xlabel('y [mm]')
 ylabel('z [mm]')
-% zlabel('z [mm]')
 ylim([80 110])
+% zlabel('z [mm]')
 legend('trajectory','waypoints','fitted points')
-title('quintic polynomial trajectory')
+title('Bspline trajectory')
 
 % figure
 % plot(tvec,sqrt(sd(2,:).^2+sd(3,:).^2))
@@ -73,7 +71,6 @@ title('quintic polynomial trajectory')
 
 figure
 plot(tvec,sd,'LineWidth',1)
-legend('vx','vy','vz');
-xlabel('time [sec]')
 ylabel('velocity [mm/s]')
-title('linear velocity')
+legend('vx','vy','vz');
+title('velocity')
